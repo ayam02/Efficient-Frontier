338d40306ee113371358d08ba80521054e593cb8
MikeGibb7 <119349139+MikeGibb7@users.noreply.github.com>
2024-03-14 14:11:53 -0400

    No longer generates random portfolios, Max and min weightings are now able to be changed

--- EfficientFrontier.py
+++ EfficientFrontier.py
@@ -31,7 +31,8 @@ def fetch_stock_data(symbols, start, end):
 #end fetch_stock_data
 
 
-'''
+'''+
+
 Function Name: plot_portfolios
 Inputs: weight_arr - an list of all arrays of all weightings for each portfolio
         port_returns - list of all portfolios returns
@@ -42,22 +43,10 @@ Desc: Accepts all information about each portfolio that has been created, then p
       and enables hover feature for mouse hovering over each portfolio(so weights for that specified
       portfolio returns are visible). Displays Graph
 '''
-def plot_portfolios(weight_arr, port_returns, port_std, sym, opt_weights, opt_vol, opt_returns):
+def plot_portfolios(sym, opt_weights, opt_vol, opt_returns):
     plt.figure(figsize = (12,6))                             # Set size of graph
     
-    # Enable cursor hover annotations
-
-    weight_str = []   # Create array to store labels
-    weights = ""      # Create and initialize empty variable to store weight                             
-    for i in weight_arr:         # Runs for every array of weights in the weight list
-        for j in range(len(i)):  # Runs for the amount of weights there are in the array
-            weights = weights + (sym[j] + " " + "{:.2f}".format(i[j]) + " ")  # Formats a label which is readable
-            #end for
-        weight_str.append(weights) # Adds the label to the new Array of labels
-        weights = ""               # Empty storage string
-        #end for
-    plt.scatter(port_std,port_returns,c = (port_returns / port_std), marker='o')# Plots all points on the graph, also calcs Sharpe ratio
-    
+    weights = ""
     opt_weight_str = []
     for i in opt_weights:         # Runs for every array of weights in the weight list
         for j in range(len(i)):  # Runs for the amount of weights there are in the array
@@ -73,10 +62,7 @@ def plot_portfolios(weight_arr, port_returns, port_std, sym, opt_weights, opt_vo
     def on_hover(sel):
         index = sel.target.index
         x, y = sel.target
-        if sel.artist == opt_points:
-            weight = opt_weight_str[index]
-        else:
-            weight = weight_str[index]
+        weight = opt_weight_str[index]
         sel.annotation.set_text(weight)
 
     # Connect the hover function to the cursor
@@ -103,56 +89,6 @@ def calculate_daily_returns(stock_data):
 
 
 '''
-Function Name: get_random_weights
-Inputs: df - array containing the data for the stocks
-Returns: weights - array of random allocation of weights
-Desc: Generates random weights for each stock in the portfolio
-'''
-def get_random_weights(num_stocks):
-  weights = np.random.random(num_stocks) #Generate random numbers for weights
-  weights /= weights.sum()                    #Normalizes weights
-  return weights
-#end get_random_weights
-
-
-'''
-Function Name: gen_portfolio
-Inputs: df - array containing the adjusted closes of all stocks
-Returns: port_returns - A list containing all the returns from the random portfolios
-         port_std - A list containing all the Standard Deviations from the random portfolios
-         weight_arr - A list containg arrays of all the weights for the random portfolios
-Desc: Used to generate a selected number of random portfolios and return all the portfolios
-      standard deviation, portfolios returns, and the weights that got those
-'''
-def gen_portfolio(df):
-    daily_returns = calculate_daily_returns(df) # Retrive the daily returns of each of the stocks
-    returns = np.array(pow(np.prod(daily_returns), 252/len(daily_returns)) - 1)
-    port_returns = []  #create lists to store the portfolios that will be generated
-    port_std = []
-    weight_arr = []
-
-    for i in range(500): #generates the number in range portfolios
-        weights = get_random_weights(len(df.columns)) #gets a random weight allocation
-        weights = np.array(weights)                   #converts weights to a numpy array
-        weight_arr.append(weights)                    #adds the weights to the storage of all weight combos
-        newReturns = np.sum(np.dot(returns,weights)) #calculates portfolio returns, uses average returns for each stock and multiplies
-                                                               #by the random weight allocated for it, multiplied by 252 trading days
-        stdev = np.sqrt(np.dot(np.dot(daily_returns.cov() * 252, weights), weights.T)) #calculates portfolio standard deviation, multiplys covalence
-                                                                                       #matrix of returns by 252 trading days, multiplys by the weights
-                                                                                       #and then the transposed weight array to get variance, square rooted to get stdev
-        port_returns.append(newReturns) #adds returns and stdev to storage arrays
-        port_std.append(stdev)
-        #end for
-    
-    weight_arr = np.array(weight_arr)    #converts storage arrays to numpy arrays
-    port_returns = np.array(port_returns)
-    port_std = np.array(port_std)
-
-    return port_returns, port_std, weight_arr
-#end gen_portfolio
-
-
-'''
 Function Name: minimize_sharpe
 Inputs: weights - array of weights
 Returns: Negative portfolios stats at weights sharpe ratio
@@ -175,6 +111,16 @@ def minimize_vol(weights):
     return vol
 #end minimize_vol
 
+def minimize_ret(weights):
+    ret = portfolio_stats(weights)['return']
+    return ret
+
+def maximize_ret(weights):
+    returns = calculate_daily_returns(stock_data) # get daily returns
+    returns = pow(np.prod(returns), 252/len(returns)) - 1 # Average annualised returns for each stock
+    ret = returns.max() - portfolio_stats(weights)['return']
+    return ret
+
 '''
 Function Name: portfolio_stats
 Inputs: weights
@@ -196,18 +142,34 @@ def portfolio_stats(weights):
 
 '''
 Function Name: get_frontier
-Inputs: num_assets - the number of stocks
+Inputs: num_assets - the number of stocks bounds - 
 Returns: list of arrays of optimal weights
 Desc: gets the returns and then runs an optimization algorithm in order to
       get minimal volatility associated with each level of returns between
       the max returns and min returns
 '''
-def gen_frontier(num_assets):
+def gen_frontier(num_assets, bounds):
     returns = calculate_daily_returns(stock_data) # get daily returns
     returns = pow(np.prod(returns), 252/len(returns)) - 1 # Average annualised returns for each stock
-    target_ret = np.linspace(returns.min(), returns.max(), 50) #create 50 points between the max and min values
-    bounds = tuple((0,1) for x in range(num_assets)) # set max and min weights
+    
     initializer = num_assets * [1./num_assets,] #set even initial weight for each asset
+    constraints = ({'type':'eq','fun': lambda x: np.sum(x) - 1})
+    for i in range(50):
+        
+        min_ret=optimize.minimize(minimize_ret, #the function we are trying to minimize
+                                 initializer,       #set the starting bounds
+                                 method = 'SLSQP',  #the method that will be used for optimization
+                                 bounds = bounds,   #set the max and min value of each weight
+                                 constraints = constraints) #sets the constraints for which the minimize_vol will be minimized
+        max_ret=optimize.minimize(maximize_ret, #the function we are trying to minimize
+                                 initializer,       #set the starting bounds
+                                 method = 'SLSQP',  #the method that will be used for optimization
+                                 bounds = bounds,   #set the max and min value of each weight
+                                 constraints = constraints) #sets the constraints for which the minimize_vol will be minimized
+    min_ret = min_ret['fun']
+    max_ret = returns.max() - max_ret['fun']
+
+    target_ret = np.linspace(min_ret, max_ret, 50) #create 50 points between the max and min values
     weights = [] #create storage for weights
     for target in target_ret: #runs for every point in the 50 targets
         #create a function to find when the minimize function at weights is equal to the target
@@ -242,6 +204,20 @@ def get_optimals(weights):
     return np.array(stdevs), np.array(return_arr)
 #end get_optimals
 
+def get_weights(symbols, min_weight, max_weight):
+    weight_bounds = list((min_weight, max_weight) for x in range(len(symbols)))
+
+    c = input("change max and min weights, input y to change ")
+    if(c == "y"):
+        for i in range(len(symbols)):
+            c = input("change max and min weights for " + symbols[i] + "? input y to change ")
+            if(c == "y"):
+                min_weight = input("Put Min ")
+                max_weight = input("Put max ")
+                weight_bounds[i] = (min_weight, max_weight)
+
+    return tuple(weight_bounds)
+
 '''
 Function Name: main
 Desc: Main method, creates stock list, start and end dates, gets stock data, generates random portfolios
@@ -251,17 +227,20 @@ def main():
 
     symbols = ["AAPL", "META", "MSFT", "GOOGL", "AMD", "KO", "J", "ALLY", "BMO"] # Add tickers you would like to be in the portfolios
     # Date range
-    start = '2023-01-01'
+    min_weight = 0.02
+    max_weight = 0.5
+    
+    start = '2014-01-01'
     end = '2024-01-01'
 
     global stock_data
     stock_data = fetch_stock_data(symbols, start, end)           # get adjusted closes of stock data in an array
     
-    returns, stdev, weights = gen_portfolio(stock_data)          # get arrays of the random portfolios that were generated
+    weight_bounds = get_weights(symbols, min_weight, max_weight)
     
-    opt_weights = gen_frontier(len(symbols))         #get the optimal weights
+    opt_weights = gen_frontier(len(symbols), weight_bounds)         #get the optimal weights
     optimal_vol, opt_rets = get_optimals(opt_weights)#get the optimal volatilities and returns
-    plot_portfolios(weights, returns, stdev, symbols, opt_weights, optimal_vol, opt_rets)            # plot the portfolios on the graph
+    plot_portfolios(symbols, opt_weights, optimal_vol, opt_rets)            # plot the portfolios on the graph
 
 #end main
     
